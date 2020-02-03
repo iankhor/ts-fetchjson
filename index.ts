ca2dbe120531db92680177f74da9937c106993a9
iankhor <iankhorbc.thecoder@gmail.com>
2020-02-03 23:33:38 +1100

    add types to function
    add gitignore

--- index.ts
+++ index.ts
@@ -2,6 +2,28 @@ import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
-axios.get(url).then(({ data }) => {
-  console.log(data);
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+axios.get(url).then(response => {
+  const todo = response.data as Todo;
+
+  const id = todo.id;
+  const title = todo.title;
+  const completed = todo.completed;
+
+  logger(id, title, completed);
 });
+
+const logger = (id: number, title: string, completed: boolean) => {
+  console.log(
+    `
+      Todo ID: ${id}
+      Has a title of: ${title}
+      Is finsihed?: ${completed}
+    `
+  );
+};
